65682e13e88eb271d2a9cf192db6f059e3e6b8ba
Sam Marten <sammarten@gmail.com>
2021-12-07 06:31:33 -0600

    day-07

---
+++ day-07/part_two.exs
@@ -0,0 +1,36 @@
+defmodule TheTreacheryOfWhales do
+  def run(input_path \\ "input.txt") do
+    input_path
+    |> read_input()
+    |> find_horizontal_position()
+  end
+
+  def read_input(input_path) do
+    input_path
+    |> File.read!()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def find_horizontal_position(positions) do
+    {min, max} = Enum.min_max(positions)
+    Enum.reduce(min..max, nil, &calc_min_fuel_consumption(&1, positions, &2))
+  end
+
+  def calc_min_fuel_consumption(position, positions, nil) do
+    calc_fuel_consumption(position, positions)
+  end
+
+  def calc_min_fuel_consumption(position, positions, current_min) do
+    fuel_consumption = calc_fuel_consumption(position, positions)
+    if fuel_consumption < current_min, do: fuel_consumption, else: current_min
+  end
+
+  def calc_fuel_consumption(position, positions) do
+    positions
+    |> Enum.map(fn cur_position -> Enum.sum(0..abs(cur_position - position)) end)
+    |> Enum.sum()
+  end
+end
+
+IO.inspect(TheTreacheryOfWhales.run())
