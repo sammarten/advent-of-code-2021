180c2bd7e4e445da6981c210a2ac67a6c7695d80
Sam Marten <sammarten@gmail.com>
2022-08-10 06:10:12 -0500

    day-12

--- day-12/part_two.exs
+++ day-12/part_two.exs
@@ -25,30 +25,45 @@ defmodule PassagePathing do
 
   defp determine_paths(conns) do
     "start"
-    |> move_to_next_cave(conns, [], [])
+    |> move_to_next_cave(conns, [], [], false)
     |> remove_dead_ends()
   end
 
-  defp move_to_next_cave("end", _conns, path, _visited_small_caves) do
+  defp move_to_next_cave("end", _conns, path, _visited_small_caves, _revisited_small_cave?) do
     [Enum.reverse(path) ++ ["end"]]
   end
 
-  defp move_to_next_cave(cave, conns, path, visited_small_caves) do
+  defp move_to_next_cave(cave, conns, path, visited_small_caves, revisited_small_cave?) do
     updated_path = [cave | path]
+
+    [possible_caves, updated_revisited_small_cave?] =
+      case [cave in visited_small_caves, revisited_small_cave?] do
+        [true, true] ->
+          [[], true]
+
+        [true, false] ->
+          [Map.get(conns, cave) -- visited_small_caves -- ["start"], true]
+
+        [false, true] ->
+          [Map.get(conns, cave)  -- ["start"], true]
+
+        [false, false] ->
+          [Map.get(conns, cave) -- ["start"], false]
+      end
 
     updated_visited_small_caves =
       if small_cave?(cave) do
-        [cave | visited_small_caves]
+        Enum.uniq([cave | visited_small_caves])
       else
         visited_small_caves
       end
 
-    case Map.get(conns, cave) -- visited_small_caves do
+    case possible_caves do
       [] ->
         []
 
       available_caves ->
-        Enum.flat_map(available_caves, &move_to_next_cave(&1, conns, updated_path, updated_visited_small_caves))
+        Enum.flat_map(available_caves, &move_to_next_cave(&1, conns, updated_path, updated_visited_small_caves, updated_revisited_small_cave?))
     end
   end
 
@@ -62,4 +77,3 @@ end
 paths = PassagePathing.run()
 IO.inspect(paths)
 IO.puts("#{length(paths)} paths")
-
